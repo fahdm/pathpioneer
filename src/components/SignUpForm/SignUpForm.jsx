d8560c72c9508ed11fb9bbb2da91530b35069892
gmanatov <jfexpr@gmail.com>
2024-05-17 16:10:29 -0500

    updated handleSubmit for SignUpForm with error handling

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -1,5 +1,7 @@
 import { Component } from "react";
 
+import { signUp } from '../../utilities/users-service'
+
 export default class SignUpForm extends Component {
 
     state = {
@@ -17,9 +19,21 @@ export default class SignUpForm extends Component {
         })
     }
 
-    handleSubmit = (e) => {
+    handleSubmit = async (e) => {
         e.preventDefault();
-        alert(JSON.stringify(this.state));
+        try {
+            const formData = {...this.state}
+            delete formData.error;
+            delete formData.confirm;
+            
+            const user = await signUp(formData);
+
+            console.log(user)
+
+        } catch {
+            // An error occurred 
+            this.setState({ error: 'Sign Up Failed - Try Again' });
+        }
     }
 
     render() {
