61a32dee8675d64d596d24a8251c12b6c12ce35f
gmanatov <jfexpr@gmail.com>
2024-05-17 17:42:52 -0500

    tested database create in node - its working, modified controllers/api/users.js - required user model and added error handling

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,12 +1,17 @@
+const User = require('../../models/user')
+
 module.exports = {
     create
   };
   
-  function create(req, res) {
-    res.json({
-      user: {
-        name: req.body.name,
-        email: req.body.email
-      }
-    });
+async function create(req, res) {
+  try {
+    // Add the user to the database
+    const user = await User.create(req.body);
+    
+  } catch (err) {
+    // Client will check for non-2xx status code 
+    // 400 = Bad Request
+    res.status(400).json(err);
   }
+}
