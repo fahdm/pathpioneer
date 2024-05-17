a3ba9aeb1f83c289c2c6d2dc1b42e90d2213bc42
gmanatov <jfexpr@gmail.com>
2024-05-17 01:07:00 -0500

    added config, mvc folders, installed and required dotenv

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
+
+// Always require and configure near the top
+require('dotenv').config()
    
 const app = express();
    
