90d7512007380c677dd52e5cb7aa320a9e651c61
gmanatov <jfexpr@gmail.com>
2024-05-20 16:17:57 -0500

    added middleware to protect server-side routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,12 @@ const express = require('express');
 const router = express.Router();
 const usersCtrl = require('../../controllers/api/users');
 
+// require the authorization middleware function
+const ensureLoggedIn = require('../../config/ensureLoggedIn');
+
+// Insert ensureLoggedIn on all routes that need protecting
+router.get('/check-token', ensureLoggedIn, usersCtrl.checkToken);
+
 // GET /api/users/check-token
 router.get('/check-token', usersCtrl.checkToken);
 
