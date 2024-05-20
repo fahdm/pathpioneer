34f62ed132e0c94100d477f8861b3ffe60940a2d
gmanatov <jfexpr@gmail.com>
2024-05-20 16:29:10 -0500

    fixed minor incosistencies (NewPathPage had checkToken button instead of PathIndexPage)

--- src/pages/NewPathPage/NewPathPage.jsx
+++ src/pages/NewPathPage/NewPathPage.jsx
@@ -1,17 +1,10 @@
 import Map from '../../components/Map/Map'
-import { checkToken } from '../../utilities/users-service';
 
 export default function NewPathPage() {
-
-    async function handleCheckToken(){
-        const expDate = await checkToken()
-        console.log(expDate)
-    }
 
     return (
         <>
         <h1>NewPathPage</h1>
-        <button onClick={handleCheckToken}>Check When My Login Expires</button>
         <Map/>
         </>
     );
--- src/pages/PathIndexPage/PathIndexPage.jsx
+++ src/pages/PathIndexPage/PathIndexPage.jsx
@@ -1,8 +1,17 @@
+
+import { checkToken } from '../../utilities/users-service';
+
 export default function PathIndexPage() {
+
+    async function handleCheckToken(){
+        const expDate = await checkToken()
+        console.log(expDate)
+    }
+
     return (
         <>
             <h1>PathIndexPage</h1>
-            <button>Check When My Login Expires</button>
+            <button onClick={handleCheckToken}>Check When My Login Expires</button>
         </>
     );
   }
