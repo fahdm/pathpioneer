ea0818956064fabe588ebd5a24d5ac37ff694fd7
Fahad Mubiru <fahadmubiru1@gmail.com>
2024-05-20 11:42:25 -0700

    wip: Map is rendered by NewPathPage

--- src/pages/PathIndexPage/PathIndexPage.jsx
+++ src/pages/PathIndexPage/PathIndexPage.jsx
@@ -1,9 +1,8 @@
-import Map from '../../components/Map/Map'
 export default function PathIndexPage() {
     return (
         <>
             <h1>PathIndexPage</h1>
-            <Map/>
+            
             <button>Check When My Login Expires</button>
         </>
     );
