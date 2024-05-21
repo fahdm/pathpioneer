3bc129c7459c54276fe6bd6d2556d01c2a44d99b
gmanatov <jfexpr@gmail.com>
2024-05-21 17:21:46 -0500

    commented out some redundant code

--- src/pages/PathIndexPage/PathIndexPage.jsx
+++ src/pages/PathIndexPage/PathIndexPage.jsx
@@ -1,13 +1,18 @@
-import { checkToken } from '../../utilities/users-service';
+// import { checkToken } from '../../utilities/users-service';
 export default function PathIndexPage() {
-    async function handleCheckToken(){
-        const expDate = await checkToken()
-        console.log(expDate)
-    }
+
+    // async function handleCheckToken(){
+    //     const expDate = await checkToken()
+    //     console.log(expDate)
+    // }
+
     return (
         <>
             <h1>PathIndexPage</h1>
-            <button onClick={handleCheckToken}>Check When My Login Expires</button>
+            {/* <button onClick={handleCheckToken}>Check When My Login Expires</button> */}
+
+            
+
         </>
     );
   }
