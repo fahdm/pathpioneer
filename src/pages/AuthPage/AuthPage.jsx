06b5c27ff139d710548e0629331d87946276e179
Fahad Mubiru <fahadmubiru1@gmail.com>
2024-05-23 15:24:51 -0700

    Moved Bgimage to assests folder

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import  { useState } from "react";
 import SignUpForm from "../../components/SignUpForm/SignUpForm";
 import LoginForm from "../../components/LoginForm/LoginForm";
-import "./AuthPage.css"; 
+import "./AuthPage.css";
 
 export default function AuthPage({ setUser }) {
     const [showSignUp, setShowSignUp] = useState(false);
@@ -12,15 +12,15 @@ export default function AuthPage({ setUser }) {
 
     return (
         <main className="auth-page">
-            <h1>Welcome to Our Community!</h1>
-            
+            <h1>Path Pioneer!</h1>
+            <h3> Where, will you go next?</h3>
+            <p>Sign Up to Plan Your Routes</p>
             <div className="form-container">
                 {showSignUp ? (
                     <SignUpForm setUser={setUser} />
                 ) : (
                     <LoginForm setUser={setUser} />
                 )}
-
                 <button onClick={toggleForm} className="toggle-button">
                     {showSignUp ? 'Already have an account? Log In' : "Don't have an account? Sign Up"}
                 </button>
@@ -28,3 +28,4 @@ export default function AuthPage({ setUser }) {
         </main>
     );
 }
+
