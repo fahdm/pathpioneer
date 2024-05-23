f54bfa0076a37b8566681498320d5fba3c5338d3
jeilanim <jeilandahiri2@gmail.com>
2024-05-23 13:53:33 -0400

    adds styling to pathcard

--- src/components/PathCard/PathCard.jsx
+++ src/components/PathCard/PathCard.jsx
@@ -4,16 +4,13 @@ import { Link } from 'react-router-dom';
 
 export default function PathCard({ path }) {
     return (
-        <>
-
-        <li><Link to={`/paths/${path._id}`}><div>
-        
-        <h4>{path.name} :</h4>
-        <MapThumb path={path} />
-        
-        </div></Link></li>
-
-
-        </>
+        <div className="card-container">
+          <Link to={`/paths/${path._id}`}>
+            <div>
+              <h4>{path.name} :</h4>
+              <MapThumb path={path} />
+            </div>
+          </Link>
+        </div>
     );
   }
